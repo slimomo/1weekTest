92f876b219a3c34eb6e0d75ffd0d6144265f1874
slimomo <77025167+slimomo@users.noreply.github.com>
2021-01-07 10:52:00 -0800

    testTwoDesu

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public testDesu() 
+    { 
+    
     }
 
     // Update is called once per frame
